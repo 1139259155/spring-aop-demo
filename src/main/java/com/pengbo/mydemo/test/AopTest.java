d72a9810fe0f5d11066aacce19a8dae8446e9e07
pengbo <sunshine_pb@qq.com>
2020-04-23 01:22:56 +0800

    my aop demo

--- src/main/java/com/pengbo/mydemo/test/AopTest.java
+++ src/main/java/com/pengbo/mydemo/test/AopTest.java
@@ -1,15 +1,26 @@
 package com.pengbo.mydemo.test;
 
-import com.pengbo.mydemo.AppConfig;
 import com.pengbo.mydemo.inf.UserDao;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@Configuration
+@ComponentScan("com.pengbo.mydemo")
+@EnableAspectJAutoProxy//(proxyTargetClass = true)
 public class AopTest {
     public static void main(String[] args) {
-        //System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles","true");
+        // 保存jdk动态代理proxy临时文件
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+        // 保存cglib动态代理proxy临时文件
+        // System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "cglibproxyclass");
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 初始化spring，已完成代理
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AopTest.class);
+
         UserDao userDao = context.getBean(UserDao.class);
+
         userDao.queryUserById("123");
         System.out.println();
         userDao.queryUserByName("haha");
